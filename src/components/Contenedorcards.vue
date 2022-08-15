fb69fa5d949fa7bd785b29ba57ae3e4891ee339d
andres <andres.h.leal@hotmail.com>
2022-08-15 02:13:37 -0500

    carga carrusel, productos, categorias y  filtros en el navbar

---
+++ src/components/Contenedorcards.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container contenedor-card" id="productos">
+    <div class="row">
+
+      <div class="col mt-5" v-for="producto in productos.contenido">
+        <div class="card" >
+          <img src="https://http2.mlstatic.com/D_NQ_NP_883091-MCO42569690149_072020-O.webp"
+               class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text indieflower text-center">{{producto.name}}</p>
+            <p class="precio text-center">{{producto.price}}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {useStore} from "vuex";
+import {computed, onMounted} from "vue";
+
+export default {
+  name: "Contenedorcards",
+  setup(){
+    const store = useStore();
+    const productos = computed(() => store.state.productos);
+
+    onMounted(() => {
+      store.dispatch('cargarProductos')
+    });
+
+    return {
+      productos
+    }
+
+  }
+}
+</script>
+
+<style scoped>
+
+@font-face {
+  font-family: Jose Thin;
+  src: url('../assets/fonts/JosefinSans-Thin.ttf');
+}
+
+@font-face {
+  font-family: Josefin Sans;
+  src: url('../assets/fonts/JosefinSans-Regular.ttf');
+}
+
+
+.card {
+  font-family: "Josefin Sans";
+  width: 18rem;
+  height: 65vh;
+  background-color: rgba(97, 172, 153, 0.1);
+  box-shadow: 5px 10px 18px #888888;
+  transition: transform 1s;
+}
+
+.card:hover {
+  transform: translate(0px, -20px);
+  cursor: pointer;
+  box-shadow: 5px 10px 18px #58809b;
+}
+
+.card::before {
+  transform: skewX(345deg);
+  background-color: rgba(255, 255, 255, 0.2);
+}
+
+.card-img-top {
+  padding: 20px;
+  height: 40vh;
+}
+
+.precio {
+  color: #04BF9D;
+  font-weight: bolder;
+  font-size: 18px;
+}
+
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="text-center lobster header" id="noticias">
+    <img :src="require('./../assets/imagenes background/arbol.png')" alt="" class="imagen-titulo">
+    <h1 class="titulo">MADAME CLARISSE</h1>
+
+    <ul class="redes">
+      <li style="--clr:#25d366">
+        <a href="https://api.whatsapp.com/send?phone=573028600085" target="_blank">
+          <i class="fa-brands fa-whatsapp"></i>
+        </a>
+      </li>
+      <li style="--clr:#1da1f2">
+        <a href="https://www.facebook.com/marlon.murillo.3762" target="_blank">
+          <i class="fa-brands fa-facebook-f"></i>
+        </a>
+      </li>
+      <li style="--clr:#dd2a7b">
+        <a href="https://www.instagram.com/madameclarissestore/" target="_blank">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+      </li>
+      <li style="--clr:#ff0000">
+        <a href="https://www.youtube.com/channel/UCQqhSn0yumS37gGQg-ABLEg" target="_blank">
+          <i class="fa-brands fa-youtube"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: "Header",
+  setup(){
+
+
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+@font-face {
+  font-family: lobster;
+  src: url('../assets/fonts/Lobster-Regular.ttf');
+}
+
+@font-face {
+  font-family: indieflower;
+  src: url('./../assets/fonts/IndieFlower-Regular.ttf');
+}
+
+.titulo {
+  color: rgb(97, 136, 116);
+  margin-top: 4vh;
+  font-size: 55px;
+}
+
+.header {
+  display: flex;
+  justify-content: center;
+}
+
+.imagen-titulo {
+  width: 8vw;
+  height: 15vh;
+  margin-left: 22vw;
+}
+
+.lobster {
+  font-family: lobster;
+}
+
+ul.redes {
+  margin-top: 5vh;
+  margin-left: 5vw;
+  position: relative;
+  display: flex;
+  gap: 40px;
+  list-style: none;
+}
+
+
+
+ul.redes li .fa-brands {
+  font-size: 2em;
+  -webkit-text-stroke: 2px var(--clr);
+  color: transparent;
+}
+
+
+
+
+</style>
