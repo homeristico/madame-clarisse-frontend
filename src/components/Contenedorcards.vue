c1e12261d2fe87b9d19782f494550e5844db8ee0
andres <andres.h.leal@hotmail.com>
2022-08-16 01:58:06 -0500

    created filter, pagination and search

--- src/components/Contenedorcards.vue
+++ src/components/Contenedorcards.vue
@@ -1,38 +1,108 @@
 <template>
+
+<!--  CARDS-->
+
+
+
   <div class="container contenedor-card" id="productos">
     <div class="row">
 
-      <div class="col mt-5" v-for="producto in productos.contenido">
+      <div class="col mt-5" v-for="producto in productos.contenido" v-if="productos.contenido.length > 0">
         <div class="card" >
           <img src="https://http2.mlstatic.com/D_NQ_NP_883091-MCO42569690149_072020-O.webp"
                class="card-img-top" alt="...">
           <div class="card-body">
             <p class="card-text indieflower text-center">{{producto.name}}</p>
             <p class="precio text-center">{{producto.price}}</p>
+            <div class="text-center">
+              <a class="link-card"
+
+                  :href="'https://api.whatsapp.com/send?phone=573028600085&text='+producto.name"
+                  target="_blank"
+              >
+                Pregunta me
+              </a>
+            </div>
+
+
+
           </div>
         </div>
       </div>
 
+      <SinElementos v-else></SinElementos>
+
     </div>
   </div>
+
+  <!-- FIN CARDS-->
+
+<!--  PAGINACIÓN-->
+
+  <div class="container-paginacion" v-if="productos.totalPaginas >= 1">
+    <nav aria-label="...">
+      <ul class="pagination">
+
+        <li class="page-item link-pagination" >
+          <a @click="paginacion(productos.numeroPagina - 1)" class="page-link" v-if="productos.numeroPagina > 1">
+            {{productos.numeroPagina - 1}}
+          </a>
+        </li>
+        <li class="page-item active" aria-current="page">
+          <span class="page-link bg-success">
+            {{productos.numeroPagina}}
+          </span>
+        </li>
+        <li class="page-item link-pagination">
+          <a @click="paginacion(productos.numeroPagina + 1)" class="page-link" v-if="productos.numeroPagina < productos.totalPaginas">
+            {{productos.numeroPagina + 1}}
+          </a>
+        </li>
+        <li class="page-item"><a class="page-link" > --- </a></li>
+        <li class="page-item link-pagination">
+          <a @click="paginacion(productos.totalPaginas)" class="page-link" >
+            {{productos.totalPaginas}}
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+  </div>
+  <!-- FIN PAGINACIÓN-->
 </template>
 
 <script>
 import {useStore} from "vuex";
-import {computed, onMounted} from "vue";
+import {computed, onBeforeMount, onMounted, ref} from "vue";
+import SinElementos from "@/components/SinElementos";
 
 export default {
   name: "Contenedorcards",
+  components: {
+    SinElementos
+  },
+
   setup(){
     const store = useStore();
     const productos = computed(() => store.state.productos);
 
-    onMounted(() => {
+    let name = computed(() => store.state.name);
+    let categoriaId = computed(() => store.state.categoriaId);
+    let page = computed(() => store.state.page);
+
+    onBeforeMount(() => {
       store.dispatch('cargarProductos')
     });
 
+
+
+    const paginacion = async (pagina) => {
+      store.dispatch('chagePage', pagina);
+      store.dispatch('cargarProductos',{categoriaId: categoriaId.value, name: name.value, page: page.value})
+    }
+
     return {
-      productos
+      productos, paginacion
     }
 
   }
@@ -77,6 +147,9 @@ export default {
   height: 40vh;
 }
 
+.link-card {
+  text-decoration: none;
+}
 .precio {
   color: #04BF9D;
   font-weight: bolder;
@@ -84,5 +157,19 @@ export default {
 }
 
 
+.container-paginacion {
+  display: flex;
+  justify-content: center;
+  margin-top: 10vh;
+  margin-bottom: 10vh;
+}
+
+.page-item {
+  padding: 5px;
+}
+
+.link-pagination {
+  cursor: pointer;
+}
 
 </style>
